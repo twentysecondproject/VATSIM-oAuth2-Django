0369fcaa6205bb48f49bdc4f9816288d626c1131
twentysecondproject <89699543+twentysecondproject@users.noreply.github.com>
2022-06-02 18:34:34 +0200

    Update views.py

--- VATSIM/views.py
+++ VATSIM/views.py
@@ -9,11 +9,11 @@ from django.contrib import messages
 
 # Create your views here.
 
-VATSIM_AUTH_URL= 'https://auth-dev.vatsim.net/oauth/authorize?client_id=378&redirect_uri=http%3A%2F%2F127.0.0.1%3A8000%2Foauth2%2Flogin%2Fredirect&response_type=code&scope=full_name+vatsim_details+email+country&required_scopes=full_name+vatsim_details+email+country'
-VATSIM_REDIRECT_URI='http://127.0.0.1:8000/oauth2/login/redirect'
-VATSIM_CLIENT_ID='378'
-VATSIM_CLIENT_SECRET='7eH6w5ec4Zqp2kEbeOfzhbhslmq8VywS6mfSFa05'
-VATSIM_SCOPES='full_name vatsim_details email country'
+VATSIM_AUTH_URL= ''
+VATSIM_REDIRECT_URI=''
+VATSIM_CLIENT_ID=''
+VATSIM_CLIENT_SECRET=''
+VATSIM_SCOPES=''
 
 
 
@@ -22,7 +22,6 @@ def vatsim_login(request: HttpRequest):
 
 def vatsim_login_redirect(request: HttpRequest):
   code = request.GET.get('code')
-  print(code)
   users = exchange_code(code)
 
   
@@ -45,7 +44,7 @@ def vatsim_login_redirect(request: HttpRequest):
 
       # remember to log the user into the system
       login(request,user)
-  return redirect('http://127.0.0.1:8000/')
+  return redirect('home')
 
 
 def exchange_code(code: str):
@@ -68,11 +67,9 @@ def exchange_code(code: str):
     'Authorization': 'Bearer %s' % access_token,
     'Accept': 'application/json'
   })
-  print(response)
   user = response.json()
-  print(user)
   return user
 
 def logout(request):
     request.session.flush()
-    return redirect('http://127.0.0.1:8000/')
+    return redirect('home')
